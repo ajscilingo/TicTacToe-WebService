a39f2bd76abb76442a0651c608ed3ff00d1a42be
Anthony Scilingo <ajscilingo@gmail.com>
2018-05-11 22:01:28 -0500

    Updating TicTacToeMove JPA Entity
    Continuing to sub out TicTacToeMove JPA Entity

--- src/main/java/net/scilingo/tictactoe/persistence/TicTacToeMove.java
+++ src/main/java/net/scilingo/tictactoe/persistence/TicTacToeMove.java
@@ -1,106 +1,125 @@
 package net.scilingo.tictactoe.persistence;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Calendar;
 
 @Entity
-@Table(name="MOVE")
+@Table(name = "MOVE")
 public class TicTacToeMove implements IMove, Serializable {
 
-    @Column(name="game_id")
+    @Column(name = "game_id")
     private Long gameId;
 
-    @Column(name="upper_left")
+    @Column(name = "upper_left")
     private Character upperLeft;
 
-    @Column(name="upper_middle")
+    @Column(name = "upper_middle")
     private Character upperMiddle;
 
-    @Column(name="upper_right")
+    @Column(name = "upper_right")
     private Character upperRight;
+
+    @Column(name = "middle_left")
+    private Character middleLeft;
+
+    @Column(name = "middle_middle")
+    private Character middleMiddle;
+
+    @Column(name = "middle_right")
+    private Character middleRight;
+
+    @Column(name = "lower_left")
+    private Character lowerLeft;
+
+    @Column(name = "lower_middle")
+    private Character lowerMiddle;
+
+    @Column(name = "lower_right")
+    private Character lowerRight;
+
+    @Column(name = "last_updated", updatable = false, insertable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar lastUpdated;
 
     @Override
     public Long getGameId() {
-        return null;
+        return this.gameId;
     }
 
     @Override
     public Character getUpperLeft() {
-        return null;
+        return this.upperLeft;
     }
 
     @Override
     public Character getUpperMiddle() {
-        return null;
+        return this.upperMiddle;
     }
 
     @Override
     public Character getUpperRight() {
-        return null;
+        return this.upperRight;
     }
 
     @Override
     public Character getMiddleLeft() {
-        return null;
+        return this.middleLeft;
     }
 
     @Override
     public Character getMiddleMiddle() {
-        return null;
+        return this.middleMiddle;
     }
 
     @Override
     public Character getMiddleRight() {
-        return null;
+        return this.middleRight;
     }
 
     @Override
     public Character getLowerLeft() {
-        return null;
+        return this.lowerLeft;
     }
 
     @Override
     public Character getLowerMiddle() {
-        return null;
+        return this.lowerMiddle;
     }
 
     @Override
     public Character getLowerRight() {
-        return null;
+        return this.lowerRight;
     }
 
     @Override
     public Calendar getLastUpdated() {
-        return null;
+        return this.getLastUpdated();
     }
 
     @Override
     public void setGameId(Long gameId) {
-
+        this.gameId = gameId;
     }
 
     @Override
     public void setUpperLeft(Character upperLeft) {
-
+        this.upperLeft = upperLeft;
     }
 
     @Override
     public void setUpperMiddle(Character upperMiddle) {
-
+        this.upperMiddle = upperMiddle;
     }
 
     @Override
     public void setUpperRight(Character upperRight) {
-
+        this.upperRight = upperRight;
     }
 
     @Override
     public void setMiddleLeft(Character middleLeft) {
-
+        this.middleLeft = middleLeft;
     }
 
     @Override
